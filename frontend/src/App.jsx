9dbc793bf9c527241902debfc1b24485b321d06a
ajibon153 <ajimuhfauji95@@gmail.com>
2026-02-02 15:03:10 +0700

    init frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+
+
+function App() {
+    return (
+        <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden"></div>
+    )
+}
+export default App
