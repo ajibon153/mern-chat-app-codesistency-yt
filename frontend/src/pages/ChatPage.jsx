fe68976503e1bdb2bb3001f2343eb4da7004bbab
ajibon153 <ajimuhfauji95@@gmail.com>
2026-02-03 09:04:18 +0700

    login logout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react"
+import { useAuthStore } from "../store/useAuthStore"
 
 const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
+    const { logout } = useAuthStore()
+    return (
+        <div>
+            ChatPage
+            <br />
+            <button className="auth-btn" type="submit" onClick={logout}>
+                Logout
+            </button>
+        </div>
+    )
 }
 
 export default ChatPage
